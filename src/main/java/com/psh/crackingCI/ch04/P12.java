1cfb6b1b4261a8f640726216a774e83262972567
SungHyun.Park <sunghyun.park@navercorp.com>
2018-04-29 16:08:03 +0900

    4-12

--- src/main/java/com/psh/crackingCI/ch04/P12.java
+++ src/main/java/com/psh/crackingCI/ch04/P12.java
@@ -1,58 +1,51 @@
+package com.psh.crackingCI.ch04;
+
+import com.psh.crackingCI.common.BinaryTreeNode;
+
 public class P12 {
     public static void main(String args[]) {
-        TreeNode root = new TreeNode(10);
+        BinaryTreeNode root = new BinaryTreeNode(10);
         
-        TreeNode n_1_1 = new TreeNode(5);
-        TreeNode n_1_2 = new TreeNode(11);
+        BinaryTreeNode n_1_1 = new BinaryTreeNode(5);
+        BinaryTreeNode n_1_2 = new BinaryTreeNode(11);
         
-        TreeNode n_2_1 = new TreeNode(-5);
-        TreeNode n_2_2 = new TreeNode(100);
-        TreeNode n_2_3 = new TreeNode(0);
-        TreeNode n_2_4 = new TreeNode(300);
+        BinaryTreeNode n_2_1 = new BinaryTreeNode(-5);
+        BinaryTreeNode n_2_2 = new BinaryTreeNode(100);
+        BinaryTreeNode n_2_3 = new BinaryTreeNode(0);
+        BinaryTreeNode n_2_4 = new BinaryTreeNode(300);
         
-        TreeNode n_3_1 = new TreeNode(-11);
+        BinaryTreeNode n_3_1 = new BinaryTreeNode(-11);
         
-        root.left = n_1_1;
-        root.right = n_1_2;
+        root.setLeft(n_1_1);
+        root.setRight(n_1_2);
         
-        n_1_1.left = n_2_1;
-        n_1_1.right = n_2_2;
-        n_1_2.left = n_2_3;
-        n_1_2.right = n_2_4;
-        n_2_3.left = n_3_1;
-        
+        n_1_1.setLeft(n_2_1);
+        n_1_1.setRight(n_2_2);
+        n_1_2.setLeft(n_2_3);
+        n_1_2.setRight(n_2_4);
+        n_2_3.setLeft(n_3_1);
         
         System.out.println(getNumPathsForSum(root, 10));
     }
     
-    public static int getNumPathsForSum(TreeNode root, int targetSum) {
+    public static int getNumPathsForSum(BinaryTreeNode root, int targetSum) {
         return getNumPathsForSum(root, targetSum, 0);
     }
     
-    public static int getNumPathsForSum(TreeNode n, int targetSum, int startSum) {
+    public static int getNumPathsForSum(BinaryTreeNode n, int targetSum, int startSum) {
         if (n == null) {
             return 0;
         }
         
         int res = 0;
-        if (startSum + n.data == targetSum) {
+        if (startSum + n.getVal() == targetSum) {
             res++;
         }
         
-        res += getNumPathsForSum(n.left, targetSum, startSum + n.data);
-        res += getNumPathsForSum(n.right, targetSum, startSum + n.data);
+        res += getNumPathsForSum(n.getLeft(), targetSum, startSum + n.getVal());
+        res += getNumPathsForSum(n.getParent(), targetSum, startSum + n.getVal());
         
         return res;
     }
     
-}
-
-class TreeNode {
-    int data;
-    TreeNode left;
-    TreeNode right;
-    
-    public TreeNode(int data) {
-        this.data = data;
-    }
 }
