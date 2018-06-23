bc9529d3b7209232d923aa52c7ab9ab9b889eacc
sh.park <livealive00@gmail.com>
2018-06-23 09:46:36 +0900

    10-5

---
+++ src/main/java/com/psh/crackingCI/ch10/P05.java
@@ -0,0 +1,11 @@
+package com.psh.crackingCI.ch10;
+
+public class P05 {
+    public static void main(String[] args) {
+        System.out.println(search(new String[] {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}, "ball"));
+    }
+
+    public static int search(String[] strings, String str) {
+        return -1;
+    }
+}
