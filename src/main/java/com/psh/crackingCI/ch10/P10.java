403b453e0d67fcb23d5191c607b9fd260126703a
sh.park <livealive00@gmail.com>
2018-06-28 10:12:42 +0900

    10-10

---
+++ src/main/java/com/psh/crackingCI/ch10/P10.java
@@ -0,0 +1,22 @@
+package com.psh.crackingCI.ch10;
+
+public class P10 {
+
+
+    public static void main(String[] args) {
+        int[] stream = {5,1,4,4,5,9,7,13,3};
+        Node root = null;
+        System.out.println(getRankOfNumber(root, 1)); // 0
+        System.out.println(getRankOfNumber(root, 3)); // 1
+        System.out.println(getRankOfNumber(root, 4)); // 3
+
+    }
+
+    public static int getRankOfNumber(Node node, int x) {
+        return 0;
+    }
+
+    class Node {
+
+    }
+}
